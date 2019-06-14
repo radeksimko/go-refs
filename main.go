1b15b5989e591fe6e0a756db9b70365386e61657
Radek Simko <radek.simko@gmail.com>
2019-06-14 12:15:18 +0100

    Refactor & add tests

--- main.go
+++ main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"io/ioutil"
 	"log"
-	"strings"
+
+	"github.com/radeksimko/go-refs/parser"
 )
 
 func main() {
-	fset := token.NewFileSet()
-
 	var packagePath string
 	var printFile bool
 
@@ -31,20 +26,13 @@ func main() {
 
 	filePath := flag.Arg(0)
 
-	b, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
 	log.Printf("Parsing %s ...", filePath)
-
-	src := string(b)
-
-	f, err := parser.ParseFile(fset, filePath, src, 0)
+	f, err := parser.ParseFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	refs, err := findPkgRefs(f, packagePath)
+	refs, err := parser.FindPackageReferences(f, packagePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -56,70 +44,4 @@ func main() {
 			fmt.Println(ref)
 		}
 	}
-}
-
-func findPkgRefs(f *ast.File, importPath string) ([]ast.Node, error) {
-	importName, err := findImportName(f.Imports, importPath)
-	if err != nil {
-		return nil, err
-	}
-
-	var refs = make([]ast.Node, 0)
-
-	ast.Inspect(f, func(node ast.Node) bool {
-		if node == nil {
-			return false
-		}
-
-		switch node.(type) {
-		case *ast.File:
-			return true
-		case *ast.ImportSpec:
-			// Imports are processed separately (above)
-			return false
-		case *ast.SelectorExpr:
-			selector := node.(*ast.SelectorExpr)
-			x := selector.X
-
-			ident, ok := x.(*ast.Ident)
-			if !ok {
-				return true
-			}
-
-			if ident.Name != importName {
-				return true
-			}
-
-			refs = append(refs, selector.Sel)
-		}
-
-		return true
-	})
-
-	return refs, nil
-}
-
-// This implementation does not handle case where package
-// is imported under different name without explicit alias
-// e.g. github.com/hashicorp/go-discover (imported as "discover")
-// This would require downloading the module and adding more complexity.
-func findImportName(imports []*ast.ImportSpec, importPath string) (string, error) {
-	if len(importPath) == 0 {
-		return "", fmt.Errorf("Unknown import path")
-	}
-
-	parts := strings.Split(importPath, "/")
-	importName := parts[len(parts)-1]
-	for _, imp := range imports {
-		path := strings.Trim(imp.Path.Value, "\"")
-		if path == importPath {
-			if imp.Name != nil {
-				importName = imp.Name.Name
-			}
-
-			return importName, nil
-		}
-	}
-
-	return "", fmt.Errorf("Import %q not found", importPath)
 }
---
+++ parser/parser_test.go
@@ -0,0 +1,80 @@
+package parser
+
+import (
+	"fmt"
+	"go/ast"
+	"reflect"
+	"testing"
+)
+
+func TestFindPackageReferences(t *testing.T) {
+	testContent := `package example
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("something")
+}
+`
+	f, err := parseFile("src.go", testContent)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	refs, err := FindPackageReferences(f, "fmt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedRefs := []string{"Printf"}
+	if !reflect.DeepEqual(stringifyAstNodes(refs), expectedRefs) {
+		t.Fatalf("Expected: %q\nGiven: %q\n", expectedRefs, refs)
+	}
+}
+
+func stringifyAstNodes(nodes []ast.Node) []string {
+	result := make([]string, len(nodes), len(nodes))
+	for i, node := range nodes {
+		result[i] = fmt.Sprint(node)
+	}
+	return result
+}
+
+func TestFindImportName(t *testing.T) {
+	testContent := `package example
+
+import (
+	xyz "fmt"
+	"os"
+)
+
+func main() {
+	xyz.Printf("something")
+}
+`
+	f, err := parseFile("src.go", testContent)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	aliasName, err := FindImportName(f.Imports, "fmt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedAliasName := "xyz"
+	if aliasName != expectedAliasName {
+		t.Fatalf("Expected: %q, given: %q", expectedAliasName, aliasName)
+	}
+
+	osName, err := FindImportName(f.Imports, "os")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expecteOsName := "os"
+	if osName != expecteOsName {
+		t.Fatalf("Expected: %q, given: %q", expecteOsName, osName)
+	}
+}
